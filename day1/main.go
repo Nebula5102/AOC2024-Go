751e33176d5a3cd04ddcf8c8e010db0847c207ec
Nebula5102 <nicklausb@shaw.ca>
2024-12-01 15:01:02 -0800

    first day function cleanup

--- day1/main.go
+++ day1/main.go
@@ -13,10 +13,44 @@ func main() {
 	if err != nil {
 		log.Fatal("Error reading file:",err)
 	}
+	defer fp.Close()
 
-	buf := make([]byte,14)
 	var list1 []int
 	var list2 []int
+	CreateLists(*fp, &list1, &list2)
+	PartOne(&list1,&list2)
+	PartTwo(&list1,&list2)
+}
+
+func PartOne(list1 *[]int, list2 *[]int) {
+	sort.Slice(*list1, func(i, j int) bool { return (*list1)[i] < (*list1)[j] })	
+	sort.Slice(*list2, func(i, j int) bool { return (*list2)[i] < (*list2)[j] })	
+	var sum int
+	for i := 0; i < len(*list1); i++ {
+		sum += Abs((*list1)[i] - (*list2)[i])
+	}
+	println(sum)
+}
+
+func PartTwo(list1 *[]int, list2 *[]int) {
+	var similarity int
+	var score int
+
+	for i := 0; i < len(*list1); i++ {
+		find := (*list1)[i]
+		for j:= 0; j < len(*list2); j++ {
+			if find == (*list2)[j] {
+				similarity++
+			}
+		}
+		score += find * similarity
+		similarity = 0
+	}
+	println(score)
+}
+
+func CreateLists(fp os.File, list1 *[]int, list2 *[]int) {
+	buf := make([]byte,14)
 	for {
 		n, err := fp.Read(buf)
 		if err != nil && err != io.EOF {
@@ -28,28 +62,9 @@ func main() {
 
 		int1, err := strconv.Atoi(string(buf[0:5]))
 		int2, err := strconv.Atoi(string(buf[8:13]))
-		list1 = append(list1, int1)
-		list2 = append(list2, int2)
+		*list1 = append(*list1, int1)
+		*list2 = append(*list2, int2)
 	}
-
-	sort.Slice(list1, func(i, j int) bool { return list1[i] < list1[j] })	
-	sort.Slice(list2, func(i, j int) bool { return list2[i] < list2[j] })	
-	var sum int
-	var similarity int
-	var score int
-	for i := 0; i < len(list1); i++ {
-		sum += Abs(list1[i] - list2[i])
-		find := list1[i]
-		for j:= 0; j < len(list2); j++ {
-			if find == list2[j] {
-				similarity++
-			}
-		}
-		score += find * similarity
-		similarity = 0
-	}
-	println(sum)
-	println(score)
 }
 
 func Abs(x int) int {
