56f724e6e80f22f9c80b976d3e5a041f75b0c847
Nick <nicklausb@shaw.ca>
2024-12-02 14:06:32 -0800

    Day2 complete

--- day2/main.go
+++ day2/main.go
@@ -12,7 +12,6 @@ import (
 type level struct {
 	safe bool
 	increasing bool
-	dampener int
 	row []int
 }
 
@@ -38,35 +37,39 @@ func CollectLevels(fp *os.File, lvls *[]level) {
 		lvl := level{
 			safe: true,
 			increasing: nums[0] < nums[1] && nums[1] < nums[2],
-			dampener: 0,
 			row: nums,
 		}
 		*lvls = append(*lvls, lvl)
 	}
 }
 
+func Safe(lvl level) bool{
+	prev := lvl.row[0]
+	for j, cur := range lvl.row {
+		// 0 1 5 3 4 5
+		if prev > cur && lvl.increasing {
+			lvl.safe = false
+		// 5 4 3 7 1 0
+		} else if prev < cur && !lvl.increasing {
+			lvl.safe = false
+		// 5 4 3 3 1 0
+		} else if prev == cur && j > 0 {
+			lvl.safe = false
+		// 0 1 2 3 7 8
+		} else if (prev - cur) > 3 {
+			lvl.safe = false
+		// 8 7 3 2 1 0
+		} else if (cur - prev) > 3 {
+			lvl.safe = false
+		}
+		prev = cur
+	}
+	return lvl.safe
+}
+
 func SafeReports(lvls *[]level) {
 	for i, lvl := range *lvls {
-		prev := lvl.row[0]
-		for j, cur := range lvl.row {
-			// 0 1 5 3 4 5
-			if prev > cur && lvl.increasing {
-				lvl.safe = false
-			// 5 4 3 7 1 0
-			} else if prev < cur && !lvl.increasing {
-				lvl.safe = false
-			// 5 4 3 3 1 0
-			} else if prev == cur && j > 0 {
-				lvl.safe = false
-			// 0 1 2 3 7 8
-			} else if (prev - cur) > 3 {
-				lvl.safe = false
-			// 8 7 3 2 1 0
-			} else if (cur - prev) > 3 {
-				lvl.safe = false
-			}
-			prev = cur
-		}
+		lvl.safe = Safe(lvl)
 		(*lvls)[i] = lvl
 	}	
 }
@@ -86,16 +89,31 @@ func PartTwo(lvls *[]level) {
 	for _, lvl := range *lvls {
 		if lvl.safe {
 			sum++
-		}
-		if !lvl.safe && lvl.dampener >= 1 && lvl.dampener < 2 {
-			sum++
+		} else {
+			var dampening = 0
+			for index,_ := range lvl.row {
+				removed := make([]int,0,len(lvl.row)-1)
+				removed = append(removed, lvl.row[:index]...)
+				removed = append(removed, lvl.row[index+1:]...)
+				rem := level{
+					safe: true, 
+					increasing: removed[0] < removed[1] && removed[1] < removed[2],
+					row: removed,
+				}
+				if Safe(rem) {
+					dampening++
+				}
+			}
+			if dampening == 2 || dampening == 1 {
+				sum++
+			}
 		}
 	}
 	println(sum)
 }
 
 func main () {
-	fp, err := os.Open("input.txt")
+	fp, err := os.Open("input2.txt")
 	if err != nil {
 		log.Fatal("Error: ", err)
 	}
