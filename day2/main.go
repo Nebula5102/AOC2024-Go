057393ae9b995cf8aad8a42c6714e553dcd35fc7
Nick <nicklausb@shaw.ca>
2024-12-02 11:35:57 -0800

    cleaned up first solution day2

--- day2/main.go
+++ day2/main.go
@@ -11,19 +11,13 @@ import (
 
 type level struct {
 	safe bool
-	dampener bool
-	bad int
+	increasing bool
+	dampener int
+	row []int
 }
 
-func main () {
-	fp, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatal("Error: ", err)
-	}
-	defer fp.Close()
+func CollectLevels(fp *os.File, lvls *[]level) {
 	reader := bufio.NewReader(fp)
-	var levels []level
-	
 	for {
 		buf, err := reader.ReadBytes('\n')
 		if err == io.EOF {
@@ -33,139 +27,83 @@ func main () {
 			log.Fatal("Error: ", err)
 		}
 		s := regexp.MustCompile(" ").Split(string(buf[:len(buf)-1]), 10)
-		log.Println(s)
-		var increasing bool
-		var prev int 
-		var safe = true 
-		var dampener = false
-		var hiccups int
-		for index, value := range s {
-			if index == 0 {
-				prev, err = strconv.Atoi(value)	
-				if err != nil {
-					log.Fatal("Error", err)
-				}
-				continue 
-			}
-			cur, err := strconv.Atoi(value)
-			if index == 1 {
-				if prev < cur {
-					increasing = true
-				} else {
-					increasing = false
-				}
-			}
+		var nums []int
+		for _, val := range s {
+			i, err := strconv.Atoi(val)
 			if err != nil {
-				log.Fatal("Error", err)
+				log.Fatal("Error:",err)
 			}
-			if cur > prev && !increasing {
-				println("in decreasing")
-				safe = false
-				if index < len(s)-1 {
-					future, err := strconv.Atoi(s[index+1])
-					if err != nil {
-						log.Fatal("Error: ", err)
-					}
-					if prev > future{
-						if dampener {
-							dampener = false
-							break
-						}
-						dampener = true 
-					}
-				}
-			} else if cur < prev && increasing {
-				println("in increasing")
-				safe = false
-				if index < len(s)-1 {
-					future, err := strconv.Atoi(s[index+1])
-					if err != nil {
-						log.Fatal("Error: ", err)
-					}
-					if prev < future{
-						if dampener {
-							dampener = false
-							break
-						}
-						dampener = true 
-					}
-				}
-			} else if (cur - prev) > 3 || (prev - cur) > 3 {
-				println("in 3")
-				safe = false
-				if index < len(s)-1 {
-					future, err := strconv.Atoi(s[index+1])
-					if err != nil {
-						log.Fatal("Error: ", err)
-					}
-					if (future - prev) < 3 && (future - prev) > -3{
-						if dampener {
-							dampener = false
-							break
-						}
-						dampener = true 
-					} 
-				}
-			} else if (cur - prev) < -3 || (prev - cur) < -3 {
-				println("in -3")
-				if index < len(s)-1 {
-					future, err := strconv.Atoi(s[index+1])
-					if err != nil {
-						log.Fatal("Error: ", err)
-					}
-					if (future - prev) < 3 && (future - prev) > -3{
-						if dampener {
-							dampener = false
-							break
-						}
-						dampener = true 
-					} 
-				}
-			} else if cur == prev {
-				safe = false
-				if index < len(s)-1 {
-					future, err := strconv.Atoi(s[index+1])
-					if err != nil {
-						log.Fatal("Error: ", err)
-					}
-					if prev < future && increasing{
-						if dampener {
-							dampener = false
-							break
-						}
-						dampener = true 
-					} else if prev > future && !increasing{
-						if dampener {
-							dampener = false
-							break
-						}
-						dampener = true 
-					}
-				}
+			nums = append(nums, i)
+		}
+		lvl := level{
+			safe: true,
+			increasing: nums[0] < nums[1] && nums[1] < nums[2],
+			dampener: 0,
+			row: nums,
+		}
+		*lvls = append(*lvls, lvl)
+	}
+}
+
+func SafeReports(lvls *[]level) {
+	for i, lvl := range *lvls {
+		prev := lvl.row[0]
+		for j, cur := range lvl.row {
+			// 0 1 5 3 4 5
+			if prev > cur && lvl.increasing {
+				lvl.safe = false
+			// 5 4 3 7 1 0
+			} else if prev < cur && !lvl.increasing {
+				lvl.safe = false
+			// 5 4 3 3 1 0
+			} else if prev == cur && j > 0 {
+				lvl.safe = false
+			// 0 1 2 3 7 8
+			} else if (prev - cur) > 3 {
+				lvl.safe = false
+			// 8 7 3 2 1 0
+			} else if (cur - prev) > 3 {
+				lvl.safe = false
 			}
-			println(dampener, value)
 			prev = cur
 		}
-		log.Println(s, safe, dampener)
-		cur_level := level{
-			safe: safe,
-			dampener: dampener,
-			bad: hiccups,
-		}
-		log.Println(hiccups, safe)
-		levels = append(levels,cur_level)
-	}
-	var sum int 
-	var sum2 int 
-	for _, level := range levels {
-		if level.safe {
+		(*lvls)[i] = lvl
+	}	
+}
+
+func PartOne(lvls *[]level) {
+	var sum int
+	for _, lvl := range *lvls {
+		if lvl.safe {
 			sum++
-			sum2++
-		} 
-		if level.dampener{
-			sum2++
 		}
 	}
 	println(sum)
-	println(sum2)
+}
+
+func PartTwo(lvls *[]level) {
+	var sum int
+	for _, lvl := range *lvls {
+		if lvl.safe {
+			sum++
+		}
+		if !lvl.safe && lvl.dampener >= 1 && lvl.dampener < 2 {
+			sum++
+		}
+	}
+	println(sum)
+}
+
+func main () {
+	fp, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+	
+	var levels []level
+	CollectLevels(fp, &levels) 
+	SafeReports(&levels)
+	PartOne(&levels)
+	PartTwo(&levels)
+	fp.Close()
 }
