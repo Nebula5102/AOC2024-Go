8bc575d45ed9a4644c60ccdcc808d21e84c6428a
Nick <nicklausb@shaw.ca>
2024-12-08 12:18:49 -0800

    day5 complete

--- day5/main.go
+++ day5/main.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"bufio"
 	"strings"
+	"slices"
 	"fmt"
 )
 
@@ -60,10 +61,19 @@ type rule struct {
 }
 
 func CreateRule(before int, beforepage int) rule {
-	 ruleSet := rule {
-		value: before,
-		after: []int{beforepage},
+	var ruleSet rule
+	if beforepage < 0 {
+		ruleSet = rule {
+			value: before,
+			after: []int{},
+		}
+	}else {
+		ruleSet = rule {
+			value: before,
+			after: []int{beforepage},
+		}
 	}
+
 	return ruleSet
 }
 
@@ -77,7 +87,8 @@ func CollateRules(rules **[][]int, ruleSets *[]rule) {
 				if rule.value == set[0] {
 					rule.after = append(rule.after,set[1])
 					(*ruleSets)[index] = rule
-				} else if index == len(*ruleSets) - 1 {
+					break
+				} else if index == len(*ruleSets) -1 {
 					ruleSet := CreateRule(set[0],set[1]) 
 					*ruleSets = append(*ruleSets, ruleSet)
 				}
@@ -86,22 +97,115 @@ func CollateRules(rules **[][]int, ruleSets *[]rule) {
 	}
 }
 
-func OrderCorrect(rules *[]rule, order *[]int) {
+func IsIn(is int,in *[]int) bool {
+	for _, val := range *in {
+		if val == is {
+			return true
+		}
+	}
+	return false
+}
+
+func CreateValidRules(rules *[]rule, order *[]int, validRs *[]rule) {
+	for _, val := range *order {
+		for index, rule := range *rules {
+			if val == rule.value {
+				*validRs = append(*validRs,rule)
+				break
+			} else if index == len(*rules) -1 {
+				ruleSet := CreateRule(val,-1) 
+				*validRs = append(*validRs,ruleSet)
+			}
+		}
+	}
+}
+
+func OrderCorrect(rules *[]rule, order *[]int) bool {
 	var validRs []rule
-	for _, val := range order
+	CreateValidRules(rules,order,&validRs)
+	for i := 0; i<len(validRs); i++ {
+		for _,check := range validRs[i+1:] {
+			val := false
+			if IsIn(check.value,&validRs[i].after){
+				val = true
+			}
+			if !val {
+				return false
+			}
+		}
+	}
+	return true
 }
 
 func PartOne(rules *[][]int, ordering *[][]int) {
 	var ruleSets []rule
 	CollateRules(&rules, &ruleSets)
+	var sum int
 	for _, order := range *ordering{
-		OrderCorrect(&ruleSets,&order)
+		if OrderCorrect(&ruleSets,&order) {
+			sum += (order[len(order)/2])
+		}
 	}
-	fmt.Println(ruleSets)
+	fmt.Println(sum)
+}
+
+func GetIncorrect(rules *[]rule,ordering **[][]int, incorrectOrders *[][]int) {
+	localRules := *rules
+	for _, order := range **ordering {
+		if !OrderCorrect(&localRules,&order) {
+			*incorrectOrders = append(*incorrectOrders,order)
+		}
+	}
+}
+
+func SortRules(validRs *[]rule) {
+	var order []rule
+	for _, rule := range *validRs {
+		if len(order) < 1 {
+			order = append(order, rule)
+		} else {
+			for index, orderedRule := range order {
+				if IsIn(orderedRule.value, &rule.after) {
+					order = slices.Insert(order,index,rule)
+					break
+				} else if index == len(order) - 1{
+					order = append(order, rule)
+				} 
+			}
+		}
+	}
+	*validRs = order
+}
+
+func Correct(rules *[]rule ,iOrd *[][]int, correctOrder *[][]int) {	
+	for _,order := range *iOrd {
+		var validRs []rule
+		CreateValidRules(rules,&order,&validRs)
+		SortRules(&validRs)
+		var ordering []int
+		for _,val := range validRs {
+			ordering = append(ordering, val.value)
+		}
+		*correctOrder = append(*correctOrder,ordering)
+	}
+}
+
+func PartTwo(rules *[][]int, ordering *[][]int) {
+	var ruleSets []rule
+	CollateRules(&rules, &ruleSets)
+	var iOrd [][]int
+	GetIncorrect(&ruleSets,&ordering,&iOrd)
+	var correctOrder [][]int
+	Correct(&ruleSets,&iOrd,&correctOrder)
+	var sum int
+	for _, order := range correctOrder{
+		sum += (order[len(order)/2])
+	}
+	fmt.Println(sum)
 }
 
 func main() {
-	fp, err := os.Open("input2.txt")
+	fp, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal("Error: ",err)
 	}
@@ -110,4 +214,5 @@ func main() {
 	CollatePages(fp, &rules, &ordering)
 	fp.Close()
 	PartOne(&rules,&ordering)
+	PartTwo(&rules,&ordering)
 }
