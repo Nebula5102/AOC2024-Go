8ec411aaa2e22afb05305db04ad25c2ad42cb377
Nick <nicklausb@shaw.ca>
2024-12-16 22:39:46 -0800

    permutations work, now time to multiply and divid

--- day7/main.go
+++ day7/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"bytes"
 	"strconv"
 )
 
@@ -44,63 +45,54 @@ func GetExpressionVariables(fp *os.File,expressions *[][]int) ([]int) {
 	return results
 }
 
-/*
-AB
-BA
-
-ABC
-ACB
-BAC
-BCA
-CAB
-CBA
-
-ABCD
-ACBD
-ADBC
-ABDC
-ACDB
-ADCB
-BACD
-BCAD
-BCDA
-BDCA
-BDAC
-BADC
-CBAD
-CABD
-CBDA
-CDBA
-CADB
-CDAB
-DABC
-DACB
-DBAC
-DBCA
-DCAB
-DCBA
-*/
-func Permutations(combination []byte, list []string) []string {
-	var starting []byte
+func Permutations(combination []byte) []string {
+	var ps []string
+	var order = make([]byte,len(combination))
+	copy(order,combination)
 	if len(combination) < 3 {
-		list = append(list, string(combination))
-		list = append(list, string(combination[1])+string(combination[0]))
-		return list
+		ps = append(ps,string(combination))
+		ps = append(ps,string(combination[1])+string(combination[0]))
+		return ps
 	} else {
 		for i := 0; i<len(combination); i++ {
-			temp := combination[0]
-			combination[0] = combination[i]
-			combination[i] = temp
-			starting = append(starting,combination[0])
-			list = Permutations(combination[1:], list)
-			for j := i*2; j < (i*2)+2; j++ {
-				list[j] = string(combination[0])+list[j]
+			copy(combination,order)
+			combination[0] = order[i]
+			combination[i] = order[0]
+			list := Permutations(combination[1:])
+			for _, combo := range list {
+				combo = string(combination[0])+combo
+				ps = append(ps, combo)
 			}
 		}
 	}
-	return list
+	return ps
 }
 
+func GetCombos(expression []int) [][]byte {
+	var combos [][]byte
+	multiply := bytes.Repeat([]byte{'*'},len(expression)-1)
+	for i := 0; i < len(multiply)+1; i++ {
+		var temp = make([]byte,len(multiply))
+		copy(temp,multiply)
+		for j:=0; j<i; j++{
+			temp[j] = '+'
+		}
+		combos = append(combos, temp)
+	}
+	return combos
+}
+
+func PartOne(results []int, expressions [][]int) {
+	for _,expression := range expressions {
+		combos := GetCombos(expression)
+		var permutations []string
+		permutations = append(permutations,string(combos[0]))
+		for _,combo := range combos[1:len(combos)-1] {
+			permutations = append(permutations,Permutations(combo)...)
+		}
+		permutations = append(permutations,string(combos[len(combos)-1]))
+	}
+}
 
 func main() {
 	fp, err := os.Open("input.txt")
@@ -110,11 +102,5 @@ func main() {
 	var expressions [][]int
 	results := GetExpressionVariables(fp, &expressions)
 	fp.Close()
-	fmt.Println(results,expressions)
-	combo := []byte{'A','B','C'}
-	var list []string
-	fmt.Println(Permutations(combo, list))
-	combo = []byte{'A','B','C','D'}
-	var lis []string
-	fmt.Println(Permutations(combo, lis))
+	PartOne(results,expressions)
 }
